6cd92cb947280ec978da5b4b15d0d73c6228bd43
Furkan Mert Mısırlı <78733884+mertmisirli@users.noreply.github.com>
2023-12-25 23:24:54 +0300

    dbContext & connection info added

---
+++ QuizMingle/Infrastructure/QuizMingle.Persistence/Context/QuizMingleDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuizMingle.Domain.Entities;
+using QuizMingle.Domain.Identity;
+using QuizMingle.Persistance.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMingle.Persistence.Context
+{
+    public class QuizMingleDbContext : IdentityDbContext
+    {
+
+        public System.Data.Entity.DbSet<Question> Questions { get; set; }
+
+        public System.Data.Entity.DbSet<Score> Scores { get; set; }
+
+        public System.Data.Entity.DbSet<Quiz> Quiz {  get; set; }
+
+        public System.Data.Entity.DbSet<UserQuiz> UserQuiz { get; set; }
+
+        protected override void OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configurations.GetString("ConnectionStrings:PostgreSQL");
+        }
+
+
+
+
+    }
+}
