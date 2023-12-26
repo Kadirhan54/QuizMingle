c4d54e6ff9c6854d60c4bf1bf8f6afea3937884c
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-26 13:31:57 +0300

    [update] identity user update

--- QuizMingle/Core/QuizMingle.Domain/Identity/User.cs
+++ QuizMingle/Core/QuizMingle.Domain/Identity/User.cs
@@ -1,20 +1,19 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Microsoft.AspNetCore.Identity;
+using QuizMingle.Domain.Common;
+using QuizMingle.Domain.Entities;
 
 namespace QuizMingle.Domain.Identity
 {
-    public class User : IdentityUser
+    public class User : IdentityUser<Guid>, IEntityBase<Guid>, ICreatedOn, IModifiedOn
     {
-        public string firstName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<UserQuiz> UserQuizzes { get; set; }
 
-        public string lastName { get; set; }
-
-        
-
-
+        public string CreatedByUserId { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset? ModifiedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
     }
 }
