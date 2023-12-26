81e3a177d1f79fcbb88132e13e8331e93d82f270
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-26 17:39:22 +0300

    [update] QuizMingleDbContext update

--- QuizMingle/Infrastructure/QuizMingle.Persistence/Context/QuizMingleDbContext.cs
+++ QuizMingle/Infrastructure/QuizMingle.Persistence/Context/QuizMingleDbContext.cs
@@ -10,30 +10,23 @@ namespace QuizMingle.Persistence.Context
     {
 
         public DbSet<Question> Questions { get; set; }
-
         public DbSet<Score> Scores { get; set; }
-
         public DbSet<Quiz> Quizzes {  get; set; }
-            
         public DbSet<UserQuiz> UserQuizzes { get; set; }
+        public DbSet<UserQuizAnswer> UserQuizAnswers { get; set; }
 
         //protected override void OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder optionsBuilder)
         //{
         //    optionsBuilder.UseNpgsql(Configurations.GetString("Team3PostgreSQLDBL"));
         //}
-
+        public QuizMingleDbContext(DbContextOptions<QuizMingleDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
             base.OnModelCreating(modelBuilder);
         }
-
-        public QuizMingleDbContext(DbContextOptions<QuizMingleDbContext> dbContextOptions) : base(dbContextOptions)
-        {
-        }
-
-
-
     }
 }
