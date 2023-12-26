17cf55daab18b0acce94ae57801f30323bd411c0
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-26 13:31:28 +0300

    [added] UserQuizAnswers added

---
+++ QuizMingle/Core/QuizMingle.Domain/Entities/UserQuizAnswers.cs
@@ -0,0 +1,19 @@
+﻿using QuizMingle.Domain.Common;
+
+namespace QuizMingle.Domain.Entities
+{
+    public class UserQuizAnswers : ICreatedOn
+    {
+        public Guid Id { get; set; }
+        public string GivenAnswer { get; set; }
+
+        // Foreign keys
+        public Guid QuestionId { get; set; }
+        public Question Question { get; set; }
+
+        public Guid UserQuizId { get; set; }
+        public UserQuiz UserQuiz { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+}
