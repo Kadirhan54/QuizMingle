1bc44b34380d07181418b506a90466011f2e9b0f
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-26 17:39:56 +0300

    [update] QuizMingle.API program cs update

--- QuizMingle/Presentation/QuizMingle.API/Program.cs
+++ QuizMingle/Presentation/QuizMingle.API/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using QuizMingle.Domain.Identity;
 using QuizMingle.Persistence.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +9,57 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-string connectionString = builder.Configuration.GetSection("Team3PostgreSQLDB").Value;
 
+
+string connectionString = builder.Configuration.GetSection("Team3PostgreSQLDB").Value;
 builder.Services.AddDbContext<QuizMingleDbContext>(options =>
 {
     options.UseNpgsql(connectionString);
+});
+
+
+
+
+
+
+
+
+builder.Services.AddIdentity<User, Role>(options =>
+{
+    // User Password Options
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 0;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    // User Username and Email Options
+    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@$";
+    options.User.RequireUniqueEmail = true;
+
+}).AddEntityFrameworkStores<QuizMingleDbContext>()
+    .AddTokenProvider<DataProtectorTokenProvider<User>>(TokenOptions.DefaultProvider);
+
+
+builder.Services.Configure<SecurityStampValidatorOptions>(options =>
+{
+    options.ValidationInterval = TimeSpan.FromMinutes(30);
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = new PathString("/Auth/Login");
+    options.LogoutPath = new PathString("/Auth/Logout");
+    options.Cookie = new CookieBuilder
+    {
+        Name = "QuizMingle",
+        HttpOnly = true,
+        SameSite = SameSiteMode.Strict,
+        SecurePolicy = CookieSecurePolicy.SameAsRequest // Always
+    };
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromDays(7);
+    options.AccessDeniedPath = new PathString("/Auth/AccessDenied");
 });
 
 
