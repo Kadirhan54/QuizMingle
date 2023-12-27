db833671cd40fca5c17d70a1971033af644ec590
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-27 17:22:08 +0300

    [Create] JWT Token and Identity Mechanism

---
+++ QuizMingle/Presentation/QuizMingle.API/Controllers/AuthController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizMingle.API.Models;
+using QuizMingle.API.Services;
+using QuizMingle.Domain.Identity;
+using QuizMingle.Persistence.Context;
+
+namespace QuizMingle.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly QuizMingleDbContext _context;
+        private readonly TokenService _tokenService;
+        public AuthController(UserManager<User> userManager, QuizMingleDbContext quizMingleDbContext, TokenService tokenService )
+        {
+            _userManager = userManager;
+            _context = quizMingleDbContext;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register(RegistrationRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _userManager.CreateAsync(
+                new User 
+                {
+                    UserName = request.Username,
+                    Email = request.Email,
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    CreatedByUserId = "halaymaster"
+                },
+                request.Password
+            );
+
+            if (result.Succeeded)
+            {
+                request.Password = "";
+                return CreatedAtAction(nameof(Register), new { email = request.Email }, request);
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<AuthResponse>> Authenticate([FromBody] AuthRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var managedUser = await _userManager.FindByEmailAsync(request.Email);
+            if (managedUser == null)
+            {
+                return BadRequest("Bad credentials");
+            }
+            var isPasswordValid = await _userManager.CheckPasswordAsync(managedUser, request.Password);
+            if (!isPasswordValid)
+            {
+                return BadRequest("Bad credentials");
+            }
+            var userInDb = _context.Users.FirstOrDefault(u => u.Email == request.Email);
+            if (userInDb is null)
+                return Unauthorized();
+
+            var accessToken = _tokenService.CreateToken(userInDb);
+            await _context.SaveChangesAsync();
+            return Ok(new AuthResponse
+            {
+                Username = userInDb.UserName,
+                Email = userInDb.Email,
+                Token = accessToken,
+            });
+        }
+    }
+}
---
+++ QuizMingle/Presentation/QuizMingle.API/Models/RegistrationRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuizMingle.API.Models
+{
+    public class RegistrationRequest
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+        [Required]
+        public string Username { get; set; } = null!;
+        [Required]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        public string LastName { get; set; } = null!;
+        [Required]
+        public string Password { get; set; } = null!;
+
+    }
+}
