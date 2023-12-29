c19f4d3776338fdc0ae185b40cb754d10e339350
Seyyit Ahmet Kılıç <seyyitahmet.kilic@gmail.com>
2023-12-29 23:06:50 +0300

    [added] create quiz and question

---
+++ QuizMingle/Presentation/QuizMingle.API/Controllers/QuizController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizMingle.API.Models;
+using QuizMingle.Domain.Entities;
+using QuizMingle.Domain.Identity;
+using QuizMingle.Persistence.Context;
+using System.Security.Claims;
+
+namespace QuizMingle.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly QuizMingleDbContext _context;
+
+        public QuizController(QuizMingleDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+        [HttpPost]
+        [Route("CreateQuiz")]
+        public async Task<IActionResult> CreateQuiz([FromBody] QuizCreateRequest quizRequest)
+        {
+            // Model doğrulaması
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Quiz nesnesi oluşturma
+            var quiz = new Quiz
+            {
+                StartTime = quizRequest.StartTime,
+                TimeLimit = quizRequest.TimeLimit,
+                Questions = new List<Question>(), // Sorular için boş liste oluştur
+                UserQuizzes = new List<UserQuiz>(), // UserQuizzes için boş liste oluştur
+                CreatedByUserId = "halaymaster"
+            };
+
+            _context.Quizzes.Add(quiz);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { Message = "Quiz başarıyla oluşturuldu", QuizId = quiz.Id });
+        }
+
+        [HttpPost]
+        [Route("AddQuestion")]
+        public async Task<IActionResult> AddQuestion([FromBody] QuestionCreateRequest questionRequest)
+        {
+            // Model doğrulaması
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var selectedQuiz = _context.Quizzes.Include(q => q.Questions)
+                          .FirstOrDefault(q => q.Id == questionRequest.QuizId);
+
+            if (selectedQuiz == null)
+            {
+                return NotFound("Quiz bulunamadı.");
+            }
+
+
+            // Soru nesnesi oluşturma
+            var question = new Question
+            {
+                Text = questionRequest.Text,
+                CorrectAnswer = questionRequest.CorrectAnswer,
+                OptionA = questionRequest.OptionA,
+                OptionB = questionRequest.OptionB,
+                OptionC = questionRequest.OptionC,
+                OptionD = questionRequest.OptionD,
+                QuizId = questionRequest.QuizId, // Quiz ile ilişkilendir
+                CreatedByUserId = "halaymaster"
+            };
+
+
+            selectedQuiz.Questions.Add(question);
+            _context.Questions.Add(question);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { Message = "Soru başarıyla eklendi", QuestionId = question.Id });
+        }
+
+    }
+}
---
+++ QuizMingle/Presentation/QuizMingle.API/Models/QuizCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using QuizMingle.Domain.Entities;
+
+namespace QuizMingle.API.Models
+{
+    public class QuizCreateRequest
+    {
+        public DateTimeOffset TimeLimit { get; set; }
+        public DateTimeOffset StartTime { get; set; }
+       
+
+    }
+}
