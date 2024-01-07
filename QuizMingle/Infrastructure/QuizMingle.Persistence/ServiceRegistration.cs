10a668057cab9862eb1eada03dff4d3ee5b17d06
kadirhan <61658086+Kadirhan54@users.noreply.github.com>
2024-01-07 12:49:15 +0300

    program.cs were simplified & extension method created for adding web services

--- QuizMingle/Infrastructure/QuizMingle.Persistence/ServiceRegistration.cs
+++ QuizMingle/Infrastructure/QuizMingle.Persistence/ServiceRegistration.cs
@@ -1,15 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using QuizMingle.Application.Repositories.QuizRepositories;
+using QuizMingle.Persistence.Context;
+using Microsoft.Extensions.Configuration;
 
 namespace StudyTimer.Persistence
 {
     public static class ServiceRegistration
     {
-        public static void AddPersistenceServices(this IServiceCollection services)
+        public static void AddPersistenceServices(this IServiceCollection services,IConfiguration configuration)
         {
             //add scoped repositories services
             services.AddScoped<IQuizReadRepository,QuizReadRepository>();
             services.AddScoped<IQuizWriteRepository, QuizWriteRepository>();
+
+            string connectionString = configuration.GetSection("Team3PostgreSQLDB").Value;
+            services.AddDbContext<QuizMingleDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
 
         }
     }
---
+++ QuizMingle/Presentation/QuizMingle.API/DependencyInjection.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using QuizMingle.API.Services;
+using QuizMingle.Domain.Identity;
+using QuizMingle.Persistence.Context;
+using QuizMingle.Persistence.Service;
+using System.Text;
+
+namespace QuizMingle.API
+{
+    public static class DependencyInjection
+    {
+        public static async Task<IServiceCollection> AddWebServices(this IServiceCollection services)
+        {
+            // Caching
+            services.AddMemoryCache();
+
+            services.AddScoped<TokenService, TokenService>();
+            services.AddScoped<ITokenService, TokenService>();
+
+            services.AddIdentityCore<User>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
+            .AddEntityFrameworkStores<QuizMingleDbContext>();
+
+            services.AddIdentityCore<User>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
+            .AddEntityFrameworkStores<QuizMingleDbContext>();
+
+            services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ClockSkew = TimeSpan.Zero,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "apiWithAuthBackend",
+                    ValidAudience = "apiWithAuthBackend",
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes("!SomethingSecret!")
+                    ),
+                };
+            });
+
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
