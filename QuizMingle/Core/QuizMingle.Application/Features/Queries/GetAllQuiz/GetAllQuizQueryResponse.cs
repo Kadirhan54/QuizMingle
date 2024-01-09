858684b9246edbc7c3c89028eefdcba12510306f
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-09 20:54:40 +0300

    [Feat]: Change Handler & Response & Request

---
+++ QuizMingle/Core/QuizMingle.Application/Features/Queries/GetAllQuiz/GetAllQuizQueryResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMingle.Application.Features.Queries.GetAllQuiz
+{
+    public class GetAllQuizQueryResponse
+    {
+        public List<Quiz> Quizzes { get; set; } = new List<Quiz>();
+
+        public class Quiz
+        {
+            public Guid Id { get; set; }
+            public DateTimeOffset StartTime { get; set; }
+            public DateTimeOffset TimeLimit { get; set; }
+
+        }
+    }
+}
